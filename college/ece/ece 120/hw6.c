3c455344e4d309991939fe17286cf819b490d6ea
j t <johnathingy@gmail.com>
2023-10-04 18:43:36 -0500

    Updated ece hw, and lsightly changed physics study guide

--- college/ece/ece 120/hw6.c
+++ college/ece/ece 120/hw6.c
@@ -8,21 +8,22 @@ int main()
     unsigned int a, b, c, d,e,k;
     unsigned int f;
     /* Print header for K-map. */
-    printf("           cd \n");
+    printf("           yz \n");
     printf("      00 01 11 10 \n");
     printf(" ______________\n");
     /* row-printing loop */
     for (a = 0; 2 > a; a = a + 1) {
         for (k=0; 2>k; k=k+1){
             b=k^a;
-            printf("ab=%u%u | ", a,b);
+            printf("wx=%u%u | ", a,b);
             /* Loop over input variable b in binary order. */
             for (c = 0; 2 > c; c = c + 1) {
             /* Loop over d in binary order.*/
                 for (e = 0; 2 > e; e = e + 1) {
                     d=e^c;    
                     // f=b&(~c)|~b&~a&~c&1; SOP FORM
-                    f= (1&(a|b|~c))&(1&(~a|b))&(1&(~b|~c)); //POS FORM
+                    // f= (1&(~a&~b))|(1&(~b&~c))|(1&(~c&~d)|(1&(~a&~d))); //POS FORM 
+                    f= ((b|c))&(1&(~a | c | ~d));
                     printf(" %u",f);
                 /* Use variables b and d to calculate *
                 * input variable c (iterated in *
